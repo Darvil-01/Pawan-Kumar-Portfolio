d671b63dc09529a570eb9319b810826043a546fe
Pawan <tiger.pawan93@gmail.com>
2025-02-11 13:37:05 +0530

    resloving confict

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ["react-router-dom"], // Remove unnecessary externals
+      external: [], // Remove unnecessary externals
     },
   },
   plugins: [react()],
