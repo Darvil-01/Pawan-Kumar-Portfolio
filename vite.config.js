016e9c0838f3b9b6de9392802ecfcbd5820f6e5e
Pawan <tiger.pawan93@gmail.com>
2025-02-11 13:33:26 +0530

    resolving conflicts

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ["react", "react-router-dom"], // Remove unnecessary externals
+      external: ["react-router-dom"], // Remove unnecessary externals
     },
   },
   plugins: [react()],
