490108e09afa45ce0482a31471080d43eb3aafc2
Pawan <tiger.pawan93@gmail.com>
2025-02-11 09:33:59 +0530

    resloving vercel conflict

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: [], // Remove unnecessary externals
+      external: ["react", "react-dom"], // Remove unnecessary externals
     },
   },
   plugins: [react()],
